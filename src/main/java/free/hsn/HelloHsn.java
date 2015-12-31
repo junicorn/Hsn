993256d0afd00716cd41631a122f062b85ff6728
Adar.w <adar-w@outlook.com>
2015-12-31 23:51:49 +0800

    Hsn

--- src/main/java/free/hsn/HelloHsn.java
+++ src/main/java/free/hsn/HelloHsn.java
@@ -1,18 +1,18 @@
 package free.hsn;
 
-import free.hsn.adaptor.impl.EchoChannelAdaptor;
+import free.hsn.adaptor.impl.PMChannelAdaptor;
 import free.hsn.core.HsnServer;
 
 public class HelloHsn {
 	
 	public static void main(String[] args) throws Exception {
-		HsnServer server = new HsnServer(10080);
+		HsnServer server = new HsnServer(10080, 200);
 		
-//		server.setChannelSelectorCount(4);
-//		server.setChannelThreadCount(8);
-//		server.addSocketOption(SocketOption.SO_REUSEADDR, true);
+//		server.setBufferPoolSize(4096);
+//		server.setChannelSelectorCount(1);
+//		server.setChannelThreadCount(1);
 		
-		server.setChannelAdaptor(EchoChannelAdaptor.class);
+		server.setChannelAdaptor(PMChannelAdaptor.class);
 		server.start();
 	}
 }
---
+++ src/main/java/free/hsn/adaptor/impl/PMChannelAdaptor.java
@@ -0,0 +1,28 @@
+package free.hsn.adaptor.impl;
+
+import free.hsn.component.ChannelSession.ChannelContext;
+
+/**
+ * Http压测Adaptor
+ */
+public class PMChannelAdaptor extends StandardChannelAdaptor {
+	
+	private static final byte[] PM_HTTP_RESPONSE;
+	static {
+		StringBuilder buf = new StringBuilder();
+		buf.append("HTTP/1.1 200 OK\r\n");
+		buf.append("Connection: keep-alive\r\n");
+		buf.append("Content-Length: 2\r\n");
+		buf.append("\r\n");
+		buf.append("ok");
+		
+		PM_HTTP_RESPONSE = buf.toString().getBytes();
+	}
+	
+	@Override
+	public void onMessage(ChannelContext channelContext) {
+		channelContext.write(PM_HTTP_RESPONSE);
+		
+		channelContext.close();
+	}
+}
